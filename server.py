ece9f2d240075d781bc1aeb5dde3ab1f5c132f0c
yoginee15 <yogineebalkawde15@gmail.com>
2021-05-23 10:58:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 PORT = 8000
-MESSAGE = "Welcome to SimpliLearn\n"
+MESSAGE = "Welcome to SimpliLearn\n to check the docker image deletion logic"
 
 app = Flask(__name__)
 
